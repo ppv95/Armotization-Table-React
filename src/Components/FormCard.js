41230c21bc130d3ae383e7f878023bf67d4e9ed8
Paul Peña Vasquez <ppv2118@gmail.com>
2020-03-22 19:36:30 -0400

    Removing the unnecesary files for the project

---
+++ src/Components/FormCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const formCard = () => {
+    return(
+        
+    )
+};
+
+export default formCard;
---
+++ src/Components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const MainPage = props => {
+  return (
+    <Button variant="contained" color="primary">
+      kLK Mundo
+    </Button>
+  );
+};
+
+export default MainPage;
